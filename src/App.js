210a27ddb152ee437168c094b00f7348248e5667
Nina Palma <ninapalmadev@gmail.com>
2023-06-14 14:44:26 -0300

    inicia projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <h1>Hello, TrybeWallet!</h1>
+    </div>
+  );
 }
 
 export default App;
